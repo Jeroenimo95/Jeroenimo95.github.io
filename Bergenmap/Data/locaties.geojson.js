dc6fe07a1017e0de876aa9b332c54f089afaaac2
Jeroenimo95 <48993146+Jeroenimo95@users.noreply.github.com>
2019-04-17 21:22:24 +0200

    initial commit

---
+++ Bergenmap/Data/locaties.geojson.js
@@ -0,0 +1,226 @@
+var json_locaties = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Vikingen Kerk",
+        "Date": "13-04-2010",
+        "Place": "Bergen",
+        "Categorie": "Monument",
+        "CustomID": "ABC123"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          5.353753566741943,
+          60.33943224740276
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Ulriken",
+        "Date": "07-04-2019",
+        "Place": "Bergen",
+        "Categorie": "Wandeling",
+        "CustomID": "123ABC"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          5.380645394325256,
+          60.378209400436006
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Uitzicht op Bergen Centrum",
+        "Date": "12-04-2019",
+        "Place": "Bergen",
+        "Categorie": "Wandeling",
+        "CustomID": "CBA123"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          5.33560037612915,
+          60.39494524661224
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Appartement",
+        "Date": "21-03-2019",
+        "Place": "Bergen",
+        "Categorie": "Home",
+        "CustomID": "123CBA"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          5.336785912513733,
+          60.373641898412686
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Bryggen",
+        "Date": "12-04-2019",
+        "Place": "Bergen",
+        "Categorie": "Siteseeing",
+        "CustomID": "ABC321"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          5.323219299316406,
+          60.39702950095425
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Løvstakken",
+        "Date": "14-04-2019",
+        "Place": "Bergen",
+        "Categorie": "Wandeling",
+        "CustomID": "321ABC"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          5.319153070449829,
+          60.360760613679034
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Viking Burials",
+        "Date": "15-04-2019",
+        "Place": "Eidfjord",
+        "Categorie": "Wandeling",
+        "CustomID": "BCA123"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          7.070217132568359,
+          60.467161667943785
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Vøringsfossen",
+        "Date": "15-04-2019",
+        "Place": "Eidfjord",
+        "Categorie": "Siteseeing",
+        "CustomID": "123BCA"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          7.253916263580322,
+          60.427569116944504
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Look like Hoth",
+        "Date": "15-04-2019",
+        "Place": "Hardangervidda",
+        "Categorie": "Siteseeing",
+        "CustomID": "CAB123"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          7.41377592086792,
+          60.38236136038573
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Malmangeruten",
+        "Date": "16-04-2019",
+        "Place": "Rosendal",
+        "Categorie": "Wandeling",
+        "CustomID": "123CAB"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          6.038532257080078,
+          59.98610861707403
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Furebergsfossen",
+        "Date": "16-04-2019",
+        "Place": "Ænes",
+        "Categorie": "Siteseeing",
+        "CustomID": "ABC213"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          6.168780326843262,
+          60.10018936914318
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Steenpark",
+        "Date": "16-04-2019",
+        "Place": "Rosendal",
+        "Categorie": "Siteseeing",
+        "CustomID": "213ABC"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          6.012439727783203,
+          59.985249938641964
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Heen en Weer Ænes",
+        "Date": "16-04-2019",
+        "Place": "Ænes",
+        "Categorie": "Transport",
+        "CustomID": "Veer1"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          5.970554351806641,
+          60.0601286919985
+        ]
+      }
+    }
+  ]
+};
