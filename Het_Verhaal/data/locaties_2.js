522ef842bf5c8f644cec6f6c8af3b59f4c8c8837
Jeroenimo95 <48993146+Jeroenimo95@users.noreply.github.com>
2019-08-16 23:15:09 +0200

    Info and data added

---
+++ Het_Verhaal/data/locaties_2.js
@@ -0,0 +1,176 @@
+var json_locaties = {
+  "type": "FeatureCollection",
+  "name": "locaties_2",
+  "crs": {
+    "type": "name",
+    "properties": {
+      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
+    }
+  },
+  "features": [{
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "text": "1",
+        "radius": 10,
+        "Name": "Bibliotheektuin"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [5.542169278659149, 51.616437549228969]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": null,
+        "marker-symbol": "",
+        "text": "2",
+        "radius": 10,
+        "Name": "Lambertuskerk"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [5.540649890899658, 51.617576884445825]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "text": "3",
+        "radius": 10,
+        "Name": "Spoorfietswandelvaarrijdkruising"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [5.534834861755371, 51.622959148555815]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "text": "4",
+        "radius": 10,
+        "Name": "Woorden en Daden"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [5.533082243739536, 51.624229162231671]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "text": "5",
+        "radius": 10,
+        "Name": "Vloek en Zucht"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [5.528161525726318, 51.626569170965773]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "text": "6",
+        "radius": 10,
+        "Name": "Infokeet Waterschap Aa Maas "
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [5.523482128453488, 51.631039138004382]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "text": "7",
+        "radius": 10,
+        "Name": "Molen Aa"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [5.509894509451842, 51.634880350210516]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "text": "8",
+        "radius": 10,
+        "Name": "Tango podium"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [5.508270461680395, 51.635365868408506]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "text": "9",
+        "radius": 10,
+        "Name": "Kilsdonkse Molen: Poeziepodium"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [5.504043102264404, 51.636931403237384]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "text": "10",
+        "radius": 10,
+        "Name": "Groentepluktuin Meulenhof"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [5.486426353454589, 51.640620212707233]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "text": "11",
+        "radius": 10,
+        "Name": "Bloemenpluktuin Op ‘t Erf"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [5.479388236999512, 51.641605620045937]
+      }
+    }
+  ]
+};
